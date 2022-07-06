15883ce3f2aa3d707506b65c7a86285e2f2edec9
Chloe Wong <70902973+chloehkwong1@users.noreply.github.com>
2022-07-06 15:01:14 +0100

    Added views, urls, router

--- react_drf_app/backend/todo/views.py
+++ react_drf_app/backend/todo/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .serializers import TodoSerializer
+from rest_framework import viewsets
+from .models import Todo
 
 # Create your views here.
+class TodoView(viewsets.ModelViewSet):
+    '''API endpoint that allows todo list items to be viewed or edited.'''
+    serializer_class = TodoSerializer
+    queryset = Todo.objects.all()
