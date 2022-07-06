98367097478e54adf8646927b71a361875863b79
Chloe Wong <70902973+chloehkwong1@users.noreply.github.com>
2022-07-06 17:05:10 +0100

    Add boilerplate code to App component

--- react_drf_app/frontend/src/App.js
+++ react_drf_app/frontend/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const todoItems = [
+  {
+    id: 1,
+    title: "Nature walk in the park",
+    description: "Visit the park with my friends",
+    completed: true,
+  },
+
+  {
+    id: 2,
+    title: "Visit",
+    description: "Got to my aunt's place",
+    completed: true,
+  },
+
+  {
+    id: 3,
+    title: "Write",
+    description: "Do an article about anthropology",
+    completed: true,
+  },
+];
+
+const App = () => (
+  <main className="content">
+    <div className="row">
+      <div className="col-md-6 col-sm-10 mx-auto p-0">
+        <div className="card p-3">
+          <ul className="list-group list-group-flush">
+            {todoItems.map((item) => (
+              <div>
+                <h1>{item.title}</h1>
+                <span>{item.description}</span>
+              </div>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
-  );
-}
+  </main>
+);
 
 export default App;
