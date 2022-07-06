4bc2b00de3931382f5a612cdb26211dad7b1ccce
Chloe Wong <70902973+chloehkwong1@users.noreply.github.com>
2022-07-06 13:15:23 +0100

    Installing corsheaders and rest_framework

--- react_drf_app/backend/backend/settings.py
+++ react_drf_app/backend/backend/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todo'
+    'todo',
+    'corsheaders',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +124,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+]
